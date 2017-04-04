2432ac6e2fa2a713aa5a612b70f0ab1ba8013daf
swinter <Sascha.Winter@Uni-Jena.de>
2017-04-04 21:59:29 +0200

    added HelloWorld and logger .idea and .eclipse to gitignore

---
+++ src/main/java/de/pax/dsa/HelloWorld.java
@@ -0,0 +1,14 @@
+package de.pax.dsa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by swinter on 04.04.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+        logger.info("Hello World!");
+    }
+}
